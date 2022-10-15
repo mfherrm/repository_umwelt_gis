79b67497fba66e3e4d78ea13a287d830e8e019f6
ToniGold <anton.d853@gmail.com>
2022-10-15 12:01:56 +0200

    Unit1b Dummy Tooltip on mouseover/out

--- js/unit1b.js
+++ js/unit1b.js
@@ -9,7 +9,28 @@ var svg = d3.select(".map")
             .attr("viewBox", [0, 0, width, height])
             //dunno seems nice
             .attr("preserveAspectRatio", "xMinYMin")
-            
+
+//Define map projection
+var projection = d3.geoMercator()
+    .translate([0, 0])
+    .scale(1);
+
+//Define path generator
+var path = d3.geoPath()
+            .projection(projection);
+
+//Create colors scheme    
+var color = d3.scaleThreshold()
+                //thresholds of data
+                .domain([10,20,50,100,250,500])
+                //either d3.schemeCOLOR or own range e.g. ['#fee5d9','#fcbba1','#fc9272','#fb6a4a','#de2d26','#a50f15']
+                .range(d3.schemeReds[6]);
+
+//Create tooltip for mouseover
+var tooltip = d3.select("body")
+                    .append("div")
+                    .attr("class","tooltip")
+                    .attr("opacity",0)
             
 //Load in GeoJSON data //Promise resolve
 d3.json("../geojson/zaf_adm1-pop_dense2020.geojson")
@@ -18,21 +39,6 @@ d3.json("../geojson/zaf_adm1-pop_dense2020.geojson")
 
 //Build Map
 function drawMap(data){
-
-    //Define map projection
-    var projection = d3.geoMercator()
-                        .translate([0, 0])
-                        .scale(1);
-
-    //Define path generator
-    var path = d3.geoPath()
-                    .projection(projection);
-    
-    //colors for population density
-    var color = d3.scaleThreshold()
-        .domain([10,20,50,100,250,500])
-        .range(['#fee5d9','#fcbba1','#fc9272','#fb6a4a','#de2d26','#a50f15']);
-
     // Calculate bounding box transforms for entire collection // bbox = [[x0,y0],[x1,y1]]
     var bbox = path.bounds(data),
         s = .92 / Math.max((bbox[1][0]-bbox[0][0])/ width, (bbox[1][1] - bbox[0][1]) / height),
@@ -43,25 +49,26 @@ function drawMap(data){
         .scale(s)
         .translate(t); 
 
-    var populationDensity = {};
+    var populationDensityData = new Map();
 
     data.features.forEach(function (d) {
-        populationDensity[d.properties.ADM0_EN] = {
-            province: +d.properties.ADM1_EN,
+        console.log(d)
+        populationDensityData[d.properties.ADM1_EN] = {
+            country: +d.properties.ADM0_EN,
             pop_density: +d.properties.pop_dense_2020_adm1,
             area_sqkm: +d.properties.Area_km2,
             total_pop: +d.properties.T_TL
         }
     });
 
-    console.log(populationDensity)
+    console.log(populationDensityData)
     //Bind data and create one path per GeoJSON feature
     svg.selectAll("path")
         .data(data.features)
         .enter()
         .append("path")
         .attr("d", path)
-        .attr("id", "province")
+        .attr("class", "province")
         //get province name  
         .attr("name", function(d) {
             return d.properties.ADM1_EN;
@@ -70,21 +77,24 @@ function drawMap(data){
         .style("fill", function(d) {
             return d.properties.pop_dense_2020_adm1 ? color(d.properties.pop_dense_2020_adm1): undefined;
         })
-        .style("stroke","white")
         //Cursor on mouseover
         .style("cursor", "pointer")
         .on("mouseover", function(d){
-            console.log(d)
-            d3.select(this)
-                .style("stroke","black")
-                .style("stroke-width",2);
-            
-            d3.select("#province")
-                .text(this.name)
+            console.log(d);
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", .9);
+            tooltip.html("d.properties.pop_dense_2020_adm1")
+                            /*
+            console.log(tooltip)
+            tooltip.transition()
+                .duration(200)
+            tooltip.html("2352345346345645645645")
+            */
         })
         .on("mouseout", function(d) {
-            d3.select(this)
-                .style("stroke","white")
-                .style("stroke-width",1)
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", 0);
         });
 }  
