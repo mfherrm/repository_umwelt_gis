a2fbdc82440e9eeed425245042475ea40133f790
mfherrm <mf.herrmann@gmx.de>
2022-11-13 21:24:23 +0100

    Worked on unit 1b and index.html

--- js/unit1b_abs_rel.js
+++ js/unit1b_abs_rel.js
@@ -4,13 +4,13 @@ let header = ["Statement","Absolute","Relative"]
 
 let questions = [
                 {question:"Approximately 83 million people live in Germany",answer:"absolute"},
-                {question:"9.2% of Kenyas total population live in the county 'Nairobi'",answer:"relative"},
+                {question:"9.2% of Kenya's total population live in the county Nairobi",answer:"relative"},
                 {question:"South Africa is approximately 3.4 times bigger than Germany",answer:"relative"},
-                {question:"South Africas province 'Gauteng' has a population density of approximately 870/km²",answer:"relative"},
+                {question:"South Africa's province Gauteng has a population density of approximately 870/km²",answer:"relative"},
                 {question:"Kenya has an area of 580,367 sq km",answer:"absolute"},
-                {question:"Germanys total male population from the ages of 50 to 54 is approximately 3.4 million ",answer:"absolute"},
-                {question:"Kenya first level administrative boundary has 47 counties",answer:"absolute"},
-                {question:"South Africa has a GINI index is roughly 0.55",answer:"relative"}
+                {question:"Germany's total male population aged 50 to 54 is approximately 3.4 million ",answer:"absolute"},
+                {question:"Kenya's first level of administrative boundaries is comprised of 47 counties",answer:"absolute"},
+                {question:"South Africa has a GINI index of roughly 55%",answer:"relative"}
                 ]
 
 createTable()
